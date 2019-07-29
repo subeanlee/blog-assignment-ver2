4d6a62081da3322011870cf88d5b5cfcab6f3270
unknown <subeando@naver.com>
2019-07-29 22:56:33 +0900

    final

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','a7r2j&2yhflemgzn80))9x!ycqwiw1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool (os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
